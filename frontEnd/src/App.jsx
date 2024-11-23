f90bc10c945f070fcce38a18d1f6fa5099ba44e8
Scottiieee <splendidhoodies@gmail.com>
2024-11-23 22:00:54 +0800

    Initial commit

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-gray-100">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-blue-500 sm:text-red-500 md:text-green-500 lg:text-yellow-500 xl:text-purple-500">
+        Hello, Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
